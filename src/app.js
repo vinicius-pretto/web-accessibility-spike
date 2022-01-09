1f252920ed1e428b2f7930727ca5227edc3382d4
Vinicius Pretto <vini.prettodasilva@gmail.com>
2022-01-09 13:36:19 -0300

    add introduction slide 5

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+const compression = require("compression");
 const express = require("express");
 const config = require("./config");
 const app = express();
 
+app.use(compression());
 app.use(express.static(config.staticDir));
 
 module.exports = app;
