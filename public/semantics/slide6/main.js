6f7a35806420b9251bbb0fe647cff480bcb1d016
Vinicius Pretto <vini.prettodasilva@gmail.com>
2022-01-11 18:51:59 -0300

    layout changes on semantics slide 6

--- public/semantics/slide6/main.js
+++ public/semantics/slide6/main.js
@@ -1,10 +1,28 @@
 document.querySelector("#btn-submit").addEventListener("click", () => {
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const errorName = document.querySelector("#error-name");
+  const errorEmail = document.querySelector("#error-email");
+
+  if (!name) {
+    errorName.textContent = "Required";
+  }
+  if (!email) {
+    errorEmail.textContent = "Required";
+  }
+
   const user = {
-    name: document.querySelector("#name").value,
-    email: document.querySelector("#email").value,
+    name,
+    email,
   };
 
+  if (!user.name || !user.email) {
+    return;
+  }
   const output = document.querySelector("#output");
   output.classList.remove("hidden");
   output.innerHTML = JSON.stringify(user, null, 2);
+
+  errorName.textContent = "";
+  errorEmail.textContent = "";
 });
